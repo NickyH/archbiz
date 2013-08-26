9d8a11a9959ec4ffaa4905b76c885bc6387a86aa
NickyH <nicky.hughes@tapestry.net>
2013-08-26 16:05:04 +1000

    added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,7 @@ RecipezyV2::Application.routes.draw do
 
 root :to => 'home#index'
 
-resources :users, :tutorials, :ingredients
-resources :recipes do
-    collection do
-      get :filter
-      get :filterbytime
-      post :add_item
-    end
-  end
-
-resources :items, :only => [:index, :new, :create]
-
-get '/login' => 'session#new'
-post '/login' => 'session#create'
-delete '/login' => 'session#destroy'
+get '/contact' => 'contact#new'
+post '/contact' => 'contact#create'
 
 end
