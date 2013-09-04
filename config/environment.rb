46e0a4f3c90164e7eb36dcbd262a77806a71f8ea
NickyH <nicky.hughes@tapestry.net>
2013-09-04 15:16:42 +1000

    modified sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,12 @@ require File.expand_path('../application', __FILE__)
 Archbiz_app::Application.initialize!
 
 # Configuration for using SendGrid on Heroku
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'seahhughesarch.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+:address        => 'smtp.sendgrid.net',
+:port           => '587',
+:authentication => :plain,
+:user_name      => ENV['SENDGRID_USERNAME'],
+:password       => ENV['SENDGRID_PASSWORD'],
+:domain         => 'heroku.com',
+:enable_starttls_auto => true
 }
