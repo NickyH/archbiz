ff0a9c8056c4f5c11c7639f321ee5ee92eb54d68
NickyH <nicky.hughes@tapestry.net>
2013-09-04 16:10:50 +1000

    commented out sendgrid settings

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,14 +1,14 @@
-class InquiriesController < ApplicationController
+# class InquiriesController < ApplicationController
 
-  def new
-    @inquiry = Inquiry.new
-  end
+#   def new
+#     @inquiry = Inquiry.new
+#   end
 
-  def create
-    @inquiry = Inquiry.new(params[:inquiry])
-    # Deliver the inquiry_email
-    Notifier.new_inquiry(@inquiry).deliver
-    render :new
-  end
+#   def create
+#     @inquiry = Inquiry.new(params[:inquiry])
+#     # Deliver the inquiry_email
+#     Notifier.new_inquiry(@inquiry).deliver
+#     render :new
+#   end
 
-end
+# end
--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,10 +1,10 @@
-class Notifier < ActionMailer::Base
-  default :from => 'any_from_address@example.com'
+# class Notifier < ActionMailer::Base
+#   default :from => 'any_from_address@example.com'
 
-  # send inquiry contact email to website owner
-  def new_inquiry(inquiry)
-    mail( :to => ENV['GMAIL_USERNAME'],
-    :subject => 'NEW INQUIRY' )
-  end
+#   # send inquiry contact email to website owner
+#   def new_inquiry(inquiry)
+#     mail( :to => ENV['GMAIL_USERNAME'],
+#     :subject => 'NEW INQUIRY' )
+#   end
 
-end
+# end
