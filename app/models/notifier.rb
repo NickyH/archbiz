5558c7bbc0009823224bf15cc251514253514ad7
NickyH <nicky.hughes@tapestry.net>
2013-09-04 17:22:49 +1000

    changed sendgrid settings

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,10 +1,10 @@
 class Notifier < ActionMailer::Base
-  default :from => 'inquiry@example.com'
+  default :from => ENV['GMAIL_USERNAME']
+  default :to => ENV['GMAIL_USERNAME']
 
   # send inquiry contact email to website owner
   def new_inquiry(inquiry)
-    mail( :to => ENV['GMAIL_USERNAME'],
-    :subject => 'NEW INQUIRY' )
+    mail( :subject => 'NEW INQUIRY', :text => 'new inquiry test' )
   end
 
 end
